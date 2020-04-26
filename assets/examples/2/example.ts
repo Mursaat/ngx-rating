8ceac54a4acfa9fb22f5c9afe9149ce1337c3f4a
Mursaat <Mursaat@users.noreply.github.com>
2020-04-26 21:17:33 +0000

    deploy: 2fc7e9a585ce91a1206ba82925f6792bf9a62869

---
+++ assets/examples/2/example.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-example-2',
+  templateUrl: './example-2.component.html',
+  styleUrls: ['./example-2.component.scss'],
+})
+export class Example2Component {
+  public starsCountExample2?: number;
+}
